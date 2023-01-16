0821f2f27cc0906ff8f372bd8c4ecbdb0e2745fb
DavidSauner <davidsauner@gmail.com>
2023-01-16 11:00:49 -0300

    Feat -- post participants

--- src/server.js
+++ src/server.js
@@ -1,19 +1,52 @@
 const PORT = 5000
 import dotenv from "dotenv"
 import express from 'express'
+import cors from "cors";
+import joi from 'joi'
+import dayjs from 'dayjs'
+import {MongoClient} from "mongodb"
 dotenv.config()
 const app = express()
 app.use(express.json())
+app.use(cors());
 
+const participantsValidation  = joi.object({
+  name : joi.string().required().min(3),
 
+})
 
+const mongoClient = new MongoClient(process.env.DATABASE_URL)
+const colectionpaticipants = db.collection("participants")
 
+try{
+  await mongoClient.connect()
+  console.log("mongo conectou")
 
+}catch(err){
 
+}
 
+const db = mongoClient.db("batepapouol");
 
+app.post("/participants", async (req, res) => {
+  const {name} = req.body
+  const {error} = participantsValidation({name},{abortEarly:false})
+  if (error) {
+    return res.status(402).send(error)
+  }
 
+  const participantislogin = colectionpaticipants.find((participant) => participant.name === name)
+  if(participantislogin){
+    return res.status(409).send("nome de usuario ja cadastrado!")
+  }
 
+  await colectionpaticipants.insertOne({name})
+
+})
+app.post("/messages", async (req, res) => {})
+app.post("/status", async (req, res) => {})
+app.get("/participants", async (req, res) => {})
+app.get("/messages", async (req, res) => {})
 
 
 
